240225145b409cca9ee95b49bb832f32e0ca8234
hiroro-work <hiroro@sonicgarden.jp>
2021-07-15 21:54:01 +0900

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,10 @@ import ReactDOMServer from "react-dom/server";
 import { createIntl, createIntlCache, IntlShape, MessageDescriptor } from "react-intl";
 import { PrimitiveType, FormatXMLElementFn, Options } from "intl-messageformat";
 
-export type useCopyTunerProps = {
-  locale: string;
-  blurbs: { [key: string]: string };
-  url?: string;
-};
-
-export const useCopyTuner = (props: useCopyTunerProps) => {
-  const { locale, blurbs, url } = props;
+const useCustomIntl = (locale, blurbs) => {
   const [intl, setIntl] = useState<IntlShape>();
 
   useEffect(() => {
-    if (!blurbs) return;
-
     const cache = createIntlCache();
     const intlOriginal = createIntl({ locale, messages: blurbs }, cache);
     const { formatMessage: formatMessageOriginal } = intlOriginal;
@@ -40,13 +31,27 @@ export const useCopyTuner = (props: useCopyTunerProps) => {
     };
 
     setIntl({ ...intlOriginal, formatMessage });
-  }, [blurbs]);
+  }, [locale, blurbs]);
+
+  return intl;
+};
+
+export type useCopyTunerProps = {
+  locale: string;
+  blurbs: { [key: string]: string };
+  url?: string;
+};
+
+export const useCopyTuner = (props: useCopyTunerProps) => {
+  const { locale, blurbs, url } = props;
+  const intl = useCustomIntl(locale, blurbs);
 
   useEffect(() => {
     const startCopyRay = async () => {
       const { start } = url && (await import("./lib/main"));
       start && start(url);
     };
+
     startCopyRay();
   }, [url]);
 
