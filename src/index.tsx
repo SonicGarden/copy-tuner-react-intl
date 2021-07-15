57d8cea4a63440fcb0e5e077bfa51483f17c3829
hiroro-work <hiroro@sonicgarden.jp>
2021-07-15 21:45:33 +0900

    add useCopyTuner

---
+++ src/index.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import ReactDOMServer from "react-dom/server";
+import { createIntl, createIntlCache, IntlShape, MessageDescriptor } from "react-intl";
+import { PrimitiveType, FormatXMLElementFn, Options } from "intl-messageformat";
+
+export type useCopyTunerProps = {
+  locale: string;
+  blurbs: { [key: string]: string };
+  url?: string;
+};
+
+export const useCopyTuner = (props: useCopyTunerProps) => {
+  const { locale, blurbs, url } = props;
+  const [intl, setIntl] = useState<IntlShape>();
+
+  useEffect(() => {
+    if (!blurbs) return;
+
+    const cache = createIntlCache();
+    const intlOriginal = createIntl({ locale, messages: blurbs }, cache);
+    const { formatMessage: formatMessageOriginal } = intlOriginal;
+    const formatMessage = (
+      descriptor: MessageDescriptor,
+      values?: Record<string, PrimitiveType | FormatXMLElementFn<string, string>>,
+      opts?: Options
+    ): any => {
+      const { id } = descriptor;
+      const orgs = formatMessageOriginal(descriptor, values, opts);
+      const nodes = Array.isArray(orgs) ? orgs : [orgs];
+
+      return (
+        <span
+          dangerouslySetInnerHTML={{
+            __html: `<!--COPYRAY ${id}-->${nodes
+              .map((node) => (typeof node === "object" ? ReactDOMServer.renderToString(node) : node))
+              .join("")}`,
+          }}
+        />
+      );
+    };
+
+    setIntl({ ...intlOriginal, formatMessage });
+  }, [blurbs]);
+
+  useEffect(() => {
+    const startCopyRay = async () => {
+      const { start } = url && (await import("./lib/main"));
+      start && start(url);
+    };
+    startCopyRay();
+  }, [url]);
+
+  return { intl };
+};
